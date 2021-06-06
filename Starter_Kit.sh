8d2ecc4d9984388776ffbd32c60628929e37a8e5
RogieTheGreat <rmbernabe@up.edu.ph>
2021-06-06 11:41:56 +0800

    Create Starter_Kit.sh
    Initial bash commands

---
+++ Starter_Kit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Bash Languange
+
+# Start Kit Command 
+
+# Just remove the comment hash to make the shell command executable
+# sudo aptitude update
+# sudo apt-get update
+# sudo aptitude upgrade
+# sudo apt-get upgrade
+# sudo aptitude install xrdp # remote display for Window User
+# sudo apt-get install xrdp # remote display for Window User
+# sudo raspi-config # then turn on VNC server # sudo ifconfig or ipconfig
+
+# sudo apt-get install git-core
